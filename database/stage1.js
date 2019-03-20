ee498cf99115a8db87f1edae0ee28991514e99c5
Jonas Busseniers <j_busseniers@hotmail.com>
2019-03-20 15:25:10 +0100

    Added foto's and coords

--- database/stage1.js
+++ database/stage1.js
@@ -4,8 +4,8 @@ var locationsNew = [
       "found": false,
       "image":"images/stage1/1.jpg",
       "coords": {
-         "lat": 51.05398,
-         "long": 3.73381
+         "lat": 50.96278,
+         "long": 3.74286
       }
     },
     {
@@ -13,64 +13,64 @@ var locationsNew = [
       "found": false,
       "image":"images/stage1/2.jpg",
       "coords": {
-        "lat": 51.05449,
-        "long": 3.73696
+        "lat": 50.964889,
+        "long": 3.751790
       }
     },    {
       "id": "1-3",
       "found": false,
       "image":"images/stage1/3.jpg",
       "coords": {
-        "lat":  51.056017 ,
-        "long": 3.740148
+        "lat":  50.963890,
+        "long": 3.754809
       }
     },    {
       "id": "1-4",
       "found": false,
       "image":"images/stage1/4.jpg",
       "coords": {
-        "lat": 51.05512,
-        "long": 3.73373
+        "lat": 50.96370,
+        "long": 3.74180
       }
     },    {
       "id": "1-5",
       "found": false,
       "image":"images/stage1/5.jpg",
       "coords": {
-        "lat": 51.05414,
-        "long": 3.7312
+        "lat": 50.96321,
+        "long": 3.73956
       }
     },    {
       "id": "1-6",
       "found": false,
       "image":"images/stage1/6.jpg",
       "coords": {
-        "lat": 51.05582,
-        "long": 3.7387
+        "lat": 50.964373,
+        "long": 3.754417
       }
     },    {
       "id": "1-7",
       "found": false,
       "image":"images/stage1/7.jpg",
       "coords": {
-        "lat": 51.05336,
-        "long": 3.73653
+        "lat": 50.963054,
+        "long": 3.747831
       }
     },    {
       "id": "1-8",
       "found": false,
       "image":"images/stage1/8.jpg",
       "coords": {
-        "lat": 51.055146,
-        "long": 3.737960
+        "lat": 50.963996,
+        "long": 3.752969
       }
     },    {
       "id": "1-9",
       "found": false,
       "image":"images/stage1/9.jpg",
       "coords": {
-        "lat": 51.05297,
-        "long": 3.73619
+        "lat": 50.96227,
+        "long": 3.74472
       }
     }
   ];
@@ -84,4 +84,4 @@ if(!locations){
 
 var order = [2,5,7,1,6,8,0,3,4];
 
-var bgImg = "url('images/stage1/happy.jpg')";
+var bgImg = "url('images/stage1/valerie.jpg')";
--- database/stage2.js
+++ database/stage2.js
@@ -4,8 +4,8 @@ var locationsNew = [
       "found": false,
       "image":"images/stage2/1.jpg",
       "coords": {
-        "lat": 51.053766,
-        "long": 3.730581
+        "lat": 50.96095,
+        "long": 3.73303
       }
     },
     {
@@ -13,64 +13,64 @@ var locationsNew = [
       "found": false,
       "image":"images/stage2/2.jpg",
       "coords": {
-        "lat": 51.054621,
-        "long": 3.729363
+        "lat": 50.95588,
+        "long": 3.72517
       }
     },    {
       "id": "2-3",
       "found": false,
       "image":"images/stage2/3.jpg",
       "coords": {
-        "lat": 51.054350,
-        "long": 3.727927
+        "lat": 50.95241,
+        "long": 3.73033
       }
     },    {
       "id": "2-4",
       "found": false,
       "image":"images/stage2/4.jpg",
       "coords": {
-        "lat": 51.053891,
-        "long": 3.730786
+        "lat": 50.96256,
+        "long": 3.73832
       }
     },    {
       "id": "2-5",
       "found": false,
       "image":"images/stage2/5.jpg",
       "coords": {
-        "lat": 51.053422,
-        "long": 3.725658
+        "lat": 50.95946,
+        "long": 3.75124
       }
     },    {
       "id": "2-6",
       "found": false,
       "image":"images/stage2/6.jpg",
       "coords": {
-        "lat": 51.054906,
-        "long": 3.728958
+        "lat": 50.95541,
+        "long": 3.72600
       }
     },    {
       "id": "2-7",
       "found": false,
       "image":"images/stage2/7.jpg",
       "coords": {
-        "lat": 51.053915,
-        "long": 3.728245
+        "lat": 50.95239,
+        "long": 3.73068
       }
     },    {
       "id": "2-8",
       "found": false,
       "image":"images/stage2/8.jpg",
       "coords": {
-        "lat": 51.054546,
-        "long": 3.729280
+        "lat": 50.95932,
+        "long": 3.72907
       }
     },    {
       "id": "2-9",
       "found": false,
       "image":"images/stage2/9.jpg",
       "coords": {
-        "lat": 51.053710,
-        "long": 3.727923
+        "lat": 50.95625,
+        "long": 3.74357
       }
     }
   ];
@@ -84,4 +84,4 @@ var locationsNew = [
 
 var order = [3,0,7,1,5,2,6,8,4];
 
-var bgImg = "url('images/stage2/30bd.jpg')";
+var bgImg = "url('images/stage2/hansenjasmien.jpg')";
